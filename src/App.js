422f30dcda0da630fc54706b4e57542e7b29fbff
Arjun Das <arjundasback515@gmail.com>
2022-09-29 01:05:48 +0600

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>this is yoga app</h1>
+      <h1 className='bg-primary'>this is yoga app</h1>
+      <h1>bootstrap install</h1>
     </div>
   );
 }
