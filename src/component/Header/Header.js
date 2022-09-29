ba0ce9a8d956cedb2ba1fdd04ab8b3169c239c71
Arjun Das <arjundasback515@gmail.com>
2022-09-29 16:44:22 +0600

    cart validation and cart responsive done

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./Header.css"
-import logo from "../../image/logo.png"
+import logo from "../image/logo.png"
+
 
 const Header = () => {
     return (
         <nav className='header-nav'>
-            <img className='img-fluid' src={logo} alt="img" />
+            <img src={logo} alt="img" />
             <h1>Yoga BD</h1>
         </nav>
     );
--- src/component/YogaCart/YogaCart.js
+++ src/component/YogaCart/YogaCart.js
@@ -7,11 +7,13 @@ const YogaCart = (props) => {
         <div className='cart-container'>
             <img src={img} alt="" />
             <div className='cart-text'>
-            <h1>{name}</h1>
+            <h3>{name}</h3>
             <p>{description}</p>
             <p>For Age: {age}</p>
             <p>Time required: {time}s</p>
+            <button>Add to cart</button>
             </div>
+            
         </div>
     );
 };
